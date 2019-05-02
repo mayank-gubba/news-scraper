a1caef74fe506ffb8d0b49fbbee6a5a1857f64ea
mayank-gubba <45769489+mayank-gubba@users.noreply.github.com>
2019-05-02 13:17:46 +0530

    Initial Commit

---
+++ pages/pages_project/pages/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.views.generic import TemplateView
+
+
+class HomePageView(TemplateView):
+    template_name = 'home.html'
