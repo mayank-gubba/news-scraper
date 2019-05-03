7906cb31b70fcb79804120db1fce2eae4671ef7d
akshatb112 <50200187+akshatb112@users.noreply.github.com>
2019-05-04 01:48:40 +0530

    Delete inshorts.py

--- DS project/inshorts.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-res=requests.get('https://inshorts.com/en/read')
-
-soup=BeautifulSoup(res.text, 'lxml')
-
-headlines=soup.find('a',{'class':'clickable'})
-print(headlines.text)
