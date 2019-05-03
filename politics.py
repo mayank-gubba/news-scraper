09805a7d13420b552af2d232910e97cb94872738
divtejbindra <50200534+divtejbindra@users.noreply.github.com>
2019-05-04 00:42:50 +0530

    Add files via upload

--- politics.py
+++ politics.py
@@ -1,56 +1,53 @@
-from bs4 import BeautifulSoup
-import requests
-import csv
+import pandas as pd
 
-source = requests.get('https://www.news18.com/politics/').text
+columns = ['headline','News_link']
+df = pd.read_csv('politics.csv', names=columns)
+f=open("newtext4.html","w")
+f.write(df.to_html())
+link=[]
+head=[]
+for x in df['News_link']:
+    link.append(x)
+for x in df['headline']:
+    head.append(x)
+def form(head_line,newslink):
+    return '<td><a href="{}">{}</a></td>'.format(newslink,head_line)
 
-csv_file= open('politics.csv','w')
-csv_writer=csv.writer(csv_file)
+new=[]
+for i in range(0,len(link)):
+    new.append(form(head[i],link[i]))
+f.close()
+file=open("newtext4.html","r+")
+m=[]
+c=0
+for i in file:
+    m.append(i)
+    c=c+1
 
-soup = BeautifulSoup(source, 'lxml')
+var=12
+for i in new:
+    m[var]=i
+    var=var+5
+dele=11
+for i in new:    
+    m.pop(dele)
+    dele=dele+4
+m.pop(5)
 
-#link1
+for i in m:
+    file.write(i)
 
-trending1=soup.find('div', class_='section-blog-left-img-list')
-topic1 = trending1.find('ul')
-top1=topic1.find('li')
+file.close()
 
-headline=top1.find('a').text
-print(headline)
-link=top1.find('a')['href']
-print(link)
-csv_writer.writerow([headline,link])
-#link2
+fi=open("newtext5.html","w")
+for i in m:
+    fi.write(i)
+fi.close()
 
-trending2=soup.find('div',class_='blog-list')
-topic2=trending2.find('div',class_='blog-list-blog')
 
-side=topic2.find('p')
 
-head2=side.find('a')['href']
-print(head2)
-link2=side.find('a').text
-print(link2)
-csv_writer.writerow([head2,link2])
-#link3
 
-source2 = requests.get('https://www.foxnews.com/politics').text
 
-soup2 = BeautifulSoup(source2,'lxml')
 
-trending3=soup2.find('div',class_='siderbar-columns')
 
-top3=trending3.find('div',class_='content article-list')
-
-side3=top3.find('div',class_='info')
-
-main3=side3.find('h4',class_='title')
-
-head3=main3.find('a').text
-print(head3)
-link3=main3.find('a')['href']
-flink=f'https://www.foxnews.com{link3}'
-print(flink)
-csv_writer.writerow([head3,flink])
-
-csv_file.close()
+    
